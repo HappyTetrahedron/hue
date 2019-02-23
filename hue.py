22316b779b193cf227eeae0b3a51fb394797feec
Aline Abler <alinea@riseup.net>
2019-02-23 11:20:52 +0100

    [breaking change] Use class for hue

--- hue.py
+++ hue.py
@@ -7,95 +7,119 @@ from xdg.BaseDirectory import *
 
 configdir = xdg_config_dirs[0]
 
-try:
-    with open(configdir + "/hue/hueconfig.yml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile)
-        ymlfile.close()
-except FileNotFoundError:
-    print("Configuration file not found! Please create your configuration. Hue will not work!")
 
-def get_scene_info(scenename):
-    for scene in cfg['scenes']:
-        if scene['scene'] == scenename:
-            return scene
-    return []
+class Hue:
+    def __init__(self, cfg=None):
+        if cfg:
+            self.cfg = cfg
+        else:
+            try:
+                with open(configdir + "/hue/hueconfig.yml", "r") as ymlfile:
+                    self.cfg = yaml.load(ymlfile)
+                    ymlfile.close()
+            except FileNotFoundError:
+                print("Configuration file not found! Please create your configuration. Hue will not work!")
+        if 'scenes' not in self.cfg:
+            self._fetch_scenes_to_config()
 
-def put_request(uri, keys):
-    jdata = json.dumps(keys)
-    r = requests.put("http://" + cfg['bridge_ip'] + "/api/" + cfg['api_key'] + "/" + uri, jdata)
+    def get_scene_info(self, scenename):
+        for scene in self.cfg['scenes']:
+            if scene['scene'] == scenename:
+                return scene
+        return {}
 
-def get_request(uri):
-    r = requests.get("http://" + cfg['bridge_ip'] + "/api/" + cfg['api_key'] + "/" + uri)
-    return json.loads(r.text)
+    def put_request(self, uri, keys):
+        jdata = json.dumps(keys)
+        r = requests.put("http://" + self.cfg['bridge_ip'] + "/api/" + self.cfg['api_key'] + "/" + uri, jdata)
 
-def get_lights(group=None):
-    if group == None:
-        r = get_request("lights")
-        lights = r.keys()
-    else:
-        r = get_request("groups/%s" % group)
-        lights = r['lights']
-    return lights
+    def get_request(self, uri):
+        r = requests.get("http://" + self.cfg['bridge_ip'] + "/api/" + self.cfg['api_key'] + "/" + uri)
+        return json.loads(r.text)
 
-def get_groups():
-    r = get_request("groups")
-    groups = list(r.keys())
-    groups.insert(0, "0")
-    return groups
+    def _fetch_scenes_to_config(self):
+        scene_list = self.get_request("scenes")
 
-def activate_scene(group, scene_key):
-    keys = {"scene":scene_key}
-    put_request("groups/%s/action" % str(group), keys)
+        scenes = []
+        for scene_key, scene in scene_list.items():
+            scene_dict = dict()
+            scene_dict['scene'] = scene['name']
+            scene_dict['group'] = scene['group'] if scene['type'] == "GroupScene" else "0"
+            scene_dict['key'] = scene_key
+            scenes.append(scene_dict)
 
-def scene(scenename):
-    sceneinfo = get_scene_info(scenename)
-    activate_scene(sceneinfo['group'], sceneinfo['key'])
+        self.cfg['scenes'] = scenes
 
-def set_brightness(group, bri):
-    keys = {"bri":bri}
-    put_request("groups/"+str(group)+"/action", keys)
+    def get_lights(self, group=None):
+        if group == None:
+            r = self.get_request("lights")
+            lights = r.keys()
+        else:
+            r = self.get_request("groups/%s" % group)
+            lights = r['lights']
+        return lights
 
-def set_light_brightness(light, bri):
-    keys = {"bri":bri}
-    put_request("lights/"+str(light)+"/state", keys)
+    def get_groups(self, ):
+        r = self.get_request("groups")
+        groups = list(r.keys())
+        groups.insert(0, "0")
+        return groups
 
-def get_light_brightness(light):
-    r = get_request("lights/"+str(light))
-    return r["state"]["bri"]
+    def activate_scene(self, group, scene_key):
+        keys = {"scene": scene_key}
+        self.put_request("groups/%s/action" % str(group), keys)
 
-def get_brightness(group):
-    r = get_request("groups/"+str(group))
-    return r["action"]["bri"]
+    def scene(self, scenename):
+        sceneinfo = self.get_scene_info(scenename)
+        self.activate_scene(sceneinfo['group'], sceneinfo['key'])
 
-def set_to_color_hsb(light, color):
-	keys = {"hue": + color[0], "sat": + color[1], "bri": color[2]}
-	put_request("lights/"+str(light)+"/state", keys)
+    def set_brightness(self, group, bri):
+        keys = {"bri":bri}
+        self.put_request("groups/"+str(group)+"/action", keys)
 
-def set_group_to_color_hsb(group, color):
-	keys = {"hue": + color[0], "sat": + color[1], "bri": color[2]}
-	put_request("groups/"+str(group)+"/action", keys)
+    def set_light_brightness(self, light, bri):
+        keys = {"bri":bri}
+        self.put_request("lights/"+str(light)+"/state", keys)
 
-def set_to_color_temp(light, color):
-	keys = {"ct": color}
-	put_request("lights/"+str(light)+"/state", keys)
+    def get_light_brightness(self, light):
+        r = self.get_request("lights/"+str(light))
+        return r["state"]["bri"]
 
-def set_group_to_color_temp(group, color):
-	keys = {"ct": color}
-	put_request("groups/"+str(group)+"/action", keys)
+    def get_brightness(self, group):
+        r = self.get_request("groups/"+str(group))
+        return r["action"]["bri"]
 
-def turn_on(light):
-    keys = {"on": True}
-    put_request("lights/"+str(light)+"/state", keys)
+    def set_to_color_hsb(self, light, color):
+        keys = {"hue": + color[0], "sat": + color[1], "bri": color[2]}
+        self.put_request("lights/"+str(light)+"/state", keys)
 
-def turn_on_group(group):
-    keys = {"on": True}
-    put_request("groups/"+str(group)+"/action", keys)
+    def set_group_to_color_hsb(self, group, color):
+        keys = {"hue": + color[0], "sat": + color[1], "bri": color[2]}
+        self.put_request("groups/"+str(group)+"/action", keys)
 
-def turn_off(light):
-    keys = {"on": False}
-    put_request("lights/"+str(light)+"/state", keys)
+    def set_to_color_temp(self, light, color):
+        keys = {"ct": color}
+        self.put_request("lights/"+str(light)+"/state", keys)
 
-def turn_off_group(group):
-    keys = {"on": False}
-    put_request("groups/"+str(group)+"/action", keys)
+    def set_group_to_color_temp(self, group, color):
+        keys = {"ct": color}
+        self.put_request("groups/"+str(group)+"/action", keys)
 
+    def turn_on(self, light):
+        keys = {"on": True}
+        self.put_request("lights/"+str(light)+"/state", keys)
+
+    def turn_on_group(self, group):
+        keys = {"on": True}
+        self.put_request("groups/"+str(group)+"/action", keys)
+
+    def turn_off(self, light):
+        keys = {"on": False}
+        self.put_request("lights/"+str(light)+"/state", keys)
+
+    def turn_off_group(self, group):
+        keys = {"on": False}
+        self.put_request("groups/"+str(group)+"/action", keys)
+
+    def is_group_on(self, group):
+        status = self.get_request('groups/{}'.format(group))
+        return status['state']['any_on']
--- huesetup.py
+++ huesetup.py
@@ -4,11 +4,14 @@ import yaml
 import json
 import requests
 import time
+import hue2
 from xdg.BaseDirectory import *
+
 
 def get_request(ip, uri):
     r = requests.get("http://" + ip + "/api/" + uri)
     return json.loads(r.text)
+
 
 def post_request(ip, uri, keys):
     jdata = json.dumps(keys)
@@ -22,16 +25,16 @@ cfg = dict()
 
 success = False
 
-while (not success):
-	print("Please enter your Hue bridge IP address:")
-	ip = input()
-	try:
-		get_request(ip, "0000")
-	except:
-		success = False
-		print("Could not resolve address. Please try again.")
-	else:
-		success = True
+while not success:
+    print("Please enter your Hue bridge IP address:")
+    ip = input()
+    try:
+        get_request(ip, "0000")
+    except:
+        success = False
+        print("Could not resolve address. Please try again.")
+    else:
+        success = True
 
 cfg['bridge_ip'] = ip
 
@@ -44,33 +47,24 @@ print("Please press the blue push link button on your Hue bridge.")
 
 success = False
 
-keys = {'devicetype':devicetype}
+keys = {'devicetype': devicetype}
+
 response = [dict()]
 
-while (not 'success' in response[0]):
-	time.sleep(1)
-	response = post_request(ip, "", keys)
-	print(response)
+while 'success' not in response[0]:
+    time.sleep(1)
+    response = post_request(ip, "", keys)
+    print(response)
 
 cfg['api_key'] = response[0]['success']['username']
 
 print("API key received")
 
-scene_list = get_request(ip, cfg['api_key'] + "/scenes")
-
-scenes = []
-for scene_key, scene in scene_list.items():
-	scene_dict = dict();
-	scene_dict['scene'] = scene['name']
-	scene_dict['group'] = 1 # TODO not sure how to find out properly
-	scene_dict['key'] = scene_key
-	scenes.append(scene_dict)
-
-cfg['scenes'] = scenes
+hue = hue2.Hue(cfg)
 
 print("Scene info collected")
 
 with open(xdg_config_dirs[0] + "/hue/hueconfig.yml", "w") as outfile:
-	yaml.dump(cfg, outfile, default_flow_style=False)
+    yaml.dump(cfg, outfile, default_flow_style=False)
 
 print("Configuration stored to " + xdg_config_dirs[0] + "/hue/hueconfig.yml")
